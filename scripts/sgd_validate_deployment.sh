68c88e6833eaf376437d7cfb3849c2c9bd4f9bfb
awesomeandrey <awesomeandrey@ukr.net>
2023-05-04 15:11:27 +0300

    Introduced 2 PSGs that segregate user access depending on user entity

--- scripts/sgd_validate_deployment.sh
+++ scripts/sgd_validate_deployment.sh
@@ -6,15 +6,21 @@ sgdFolder="manifests/sgd"
 packageXml="$sgdFolder/package/package.xml"
 destructiveChangesXml="$sgdFolder/destructiveChanges/destructiveChanges.xml"
 
-if grep -r '<members>' $sgdFolder; then
-  sfdx force:source:deploy \
-    --checkonly \
-    --target-org $targetOrgAlias \
-    --verbose \
-    --ignorewarnings \
-    --manifest "$packageXml" \
-    --postdestructivechanges "$destructiveChangesXml"
-else
-  printf "\n No source-backed components present in the package. Nothing to validate! \n"
+# Check if there are metadata changes detected;
+packageXmlLinesCount=$(wc -l < $packageXml)
+printf "\n Package XML lines count = ${packageXmlLinesCount} \n"
+destructiveChangesXmlLinesCount=$(wc -l < $destructiveChangesXml)
+printf "\n Destructive XML lines count = ${destructiveChangesXmlLinesCount} \n"
+if (($packageXmlLinesCount < 10)) && (($destructiveChangesXmlLinesCount < 10)); then
+  printf '\n There are no metadata changes detected! \n'
   exit 0
 fi
+
+# Otherwise validate deployment;
+sfdx force:source:deploy \
+  --checkonly \
+  --target-org $targetOrgAlias \
+  --verbose \
+  --ignorewarnings \
+  --manifest "$packageXml" \
+  --postdestructivechanges "$destructiveChangesXml"
