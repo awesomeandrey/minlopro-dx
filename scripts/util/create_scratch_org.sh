b8579fe79ee68716ad329183d0f25b4dce69b15b
awesomeandrey <awesomeandrey@ukr.net>
2023-11-24 21:48:12 +0200

    Created script that spins up new scratch org

---
+++ scripts/util/create_scratch_org.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# How to use:
+# - bash ./scripts/util/create_scratch_org.sh
+
+# Enable errexit option to exit on command failure
+set -e
+
+# Capture DevHub org alias;
+printf "Enter DevHub org alias:\n"
+read DEV_HUB_ALIAS
+
+# Capture Scratch Org alias;
+printf "Enter Scratch Org alias:\n"
+read SCRATCH_ORG_ALIAS
+
+echo "Spinning up scratch org [$SCRATCH_ORG_ALIAS] under [$DEV_HUB_ALIAS] dev hub org..."
+
+# Create a brand new scratch org
+sf org create scratch \
+    --target-dev-hub $DEV_HUB_ALIAS \
+    --alias $SCRATCH_ORG_ALIAS \
+    --definition-file "config/project-scratch-def.json" \
+    --set-default \
+    --edition enterprise \
+    --duration-days 30 \
+    --wait 10
+
+# Optional: install packages here.
+
+# Generate project manifest
+sf project generate manifest \
+    --source-dir "src" \
+    --name "manifests/package.xml"
+
+# Initiate full deploy to scratch org (this automatically creates Digital Experience Site)
+echo "$SCRATCH_ORG_ALIAS" | bash ./scripts/deploy/deploy.sh
+
+# Enable debug mode in scratch org by default
+bash ./scripts/util/run_apex_script.sh "$SCRATCH_ORG_ALIAS" set_up_org_admin
+
+# Publish Digital Experience Site
+sf community publish \
+  --name "DigEx" \
+  --target-org "$SCRATCH_ORG_ALIAS"
+
+# TODO - import sample data;
+
+# Reset Admin user password and display it to console
+sf org generate password --target-org $SCRATCH_ORG_ALIAS
+printf "\n----- Scratch Org Credentials -----\n"
+sf org display user --target-org $SCRATCH_ORG_ALIAS
+
+echo "Done!"
--- scripts/util/run_apex_script.sh
+++ scripts/util/run_apex_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # How to use:
-# - bash ./scripts/util/run_apex_script.sh SO enable_debug_mode
+# - bash ./scripts/util/run_apex_script.sh SO set_up_org_admin
 
 # Define constants;
 targetOrg=$1          #Mandatory parameter!
 apexScriptFileName=$2 #Mandatory parameter!
 apexScriptFilePath="scripts/apex/$2.cls"
 
-printf "\nRunning Apex Script: [$2]...\n"
+printf "\nRunning Apex Script: [$2]\n"
 sf apex run --target-org $targetOrg --file $apexScriptFilePath
