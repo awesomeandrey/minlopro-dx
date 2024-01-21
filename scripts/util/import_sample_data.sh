c645846d2808e1f7a73519e7f56f95f6dd37ac5d
awesomeandrey <awesomeandrey@ukr.net>
2024-01-21 11:37:42 +0200

    Updated scratch org generation script to import currencies info

--- scripts/util/import_sample_data.sh
+++ scripts/util/import_sample_data.sh
@@ -9,6 +9,13 @@ read -p "🔶 Enter target org alias: " TARGET_ORG_ALIAS
 
 echo "🔵 Importing sample data into [$TARGET_ORG_ALIAS] organization..."
 echo
+
+# Import Accounts and Contacts
 sf data import tree \
-  --target-org $TARGET_ORG_ALIAS \
+  --target-org "$TARGET_ORG_ALIAS" \
   --plan "config/data/sample-Account-Contact-plan.json"
+
+# Import CurrencyTypes
+ sf data import tree \
+  --target-org "$TARGET_ORG_ALIAS" \
+  --files "config/data/sample-CurrencyTypes.json"
