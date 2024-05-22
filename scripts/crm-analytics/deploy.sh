afad26927ed007d8be7bb1213939e1f5086d1fdb
awesomeandrey <awesomeandrey@ukr.net>
2024-05-22 17:57:10 +0300

    Created a scripts to 'deploy & refresh' CRMA metadata separately

---
+++ scripts/crm-analytics/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# How to use:
+# - bash ./scripts/crm-analytics/deploy.sh
+# - echo $TXT_WITH_INPUTS | bash ./scripts/crm-analytics/deploy.sh
+
+# Enable errexit option to exit on command failure
+set -e
+
+# Capture Scratch Org alias;
+read -p "🔶 Enter Scratch Org Alias: " TARGET_ORG_ALIAS
+echo "🔵 Deploying CRM Analytics assets to [$TARGET_ORG_ALIAS] org..."
+
+# Cleanup working directory
+rm -rf "build"
+
+# Copy CRMA assets to 'build' folder
+mkdir -p "build"
+cp -r "src/minlopro-crma" "build"
+
+# Add folder-level ignore file to make sure the manifest captures all metadata
+touch "build/.forceignore"
+
+# Initiate deployment
+sf project deploy start \
+  --target-org "$TARGET_ORG_ALIAS" \
+  --source-dir "build/minlopro-crma" \
+  --verbose \
+  --ignore-conflicts \
+  --ignore-warnings \
+  --wait 20
---
+++ scripts/crm-analytics/refresh.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# How to use:
+# - bash ./scripts/crm-analytics/refresh.sh
+# - echo $TXT_WITH_INPUTS | bash ./scripts/crm-analytics/refresh.sh
+
+# Enable errexit option to exit on command failure
+set -e
+
+# Capture Scratch Org alias;
+read -p "🔶 Enter Scratch Org Alias: " TARGET_ORG_ALIAS
+echo "🔵 Refreshing CRM Analytics assets from [$TARGET_ORG_ALIAS] org..."
+
+# Add folder-level ignore file to make sure the retrieval operation succeeds
+touch "src/minlopro-crma/.forceignore"
+
+# Initiate retrieval
+sf project retrieve start \
+  --target-org "$TARGET_ORG_ALIAS" \
+  --source-dir "src/minlopro-crma" \
+  --ignore-conflicts
+
+# Remove CRMA ignore file in order to let standard SF CLI push/pull commands ignore WAVE metadata
+rm -f "src/minlopro-crma/.forceignore"
