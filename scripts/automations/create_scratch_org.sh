2b2042a3ab0d2204a302c35a64ba98b53aa47690
awesomeandrey <awesomeandrey@ukr.net>
2024-10-20 22:31:16 +0300

    Minlopro: updated CI scripts to publish site only for scratch org

--- scripts/automations/create_scratch_org.sh
+++ scripts/automations/create_scratch_org.sh
@@ -56,3 +56,9 @@ echo "$SCRATCH_ORG_ALIAS" | bash ./scripts/deploy/post/run_post.sh
 
 # Import sample data
 echo "$SCRATCH_ORG_ALIAS" | bash ./scripts/util/import_sample_data.sh
+
+# Publish Digital Experience Site
+sf community publish --name "DigEx" --target-org "$SCRATCH_ORG_ALIAS" || true
+
+# Publish Experience Site for Enhanced Messaging for In-App & Web Experience
+sf community publish --name "ESW_Minlopro_DigExMessaging" --target-org "$SCRATCH_ORG_ALIAS" || true
