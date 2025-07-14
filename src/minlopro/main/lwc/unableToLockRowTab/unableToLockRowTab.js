984ebbe55b568b35222621f474d33d1c45278f74
Andrii Melnichuk <awesomeandrey@ukr.net>
2025-07-14 18:38:13 +0300

    Minlopro: UNABLE_TO_LOCK_ROW Demo (WIP)

--- src/minlopro/main/lwc/unableToLockRowTab/unableToLockRowTab.js
+++ src/minlopro/main/lwc/unableToLockRowTab/unableToLockRowTab.js
@@ -1,3 +1,56 @@
 import { LightningElement, track } from 'lwc';
+import { resolveRecordId } from 'c/utilities';
 
-export default class UnableToLockRowTab extends LightningElement {}
+import updateAccountSsnApex from '@salesforce/apex/UnableToLockRowDemoController.updateAccountSsn';
+
+export default class UnableToLockRowTab extends LightningElement {
+    @track selectedAccountId = resolveRecordId('${SF_SAMPLE_ACCOUNT_ID}');
+    @track concurrentRequests = 5;
+    @track syntheticDelay = 2000;
+    @track doApplyForUpdate = false;
+    @track loading = false;
+
+    get accountDisplayInfo() {
+        return {
+            additionalFields: ['Phone']
+        };
+    }
+
+    get accountMatchingInfo() {
+        return {
+            primaryField: { fieldPath: 'Name' },
+            additionalFields: [{ fieldPath: 'Phone' }]
+        };
+    }
+
+    get stats() {
+        return {
+            'Account ID': this.selectedAccountId,
+            'Concurrent Requests': this.concurrentRequests,
+            'Synthetic Delay': `${this.syntheticDelay}ms`,
+            'Apply FOR UPDATE Clause': this.doApplyForUpdate
+        };
+    }
+
+    handleRefreshRecord(event) {}
+
+    handleAccountLookupChange(event) {
+        const { recordId } = event.detail;
+        this.selectedAccountId = recordId || null;
+    }
+
+    handleConcurrentRequestsChange(event) {
+        const { value } = event.detail;
+        this.concurrentRequests = value;
+    }
+
+    handleSyntheticDelayChange(event) {
+        const { value } = event.detail;
+        this.syntheticDelay = value;
+    }
+
+    handleDoApplyForUpdateChange(event) {
+        const { checked } = event.detail;
+        this.doApplyForUpdate = checked;
+    }
+}
