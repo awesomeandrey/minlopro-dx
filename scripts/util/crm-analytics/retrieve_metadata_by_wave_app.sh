0b156aba61baf07e854fa03fec135fbb1c45ff9f
Andrii Melnichuk <awesomeandrey@ukr.net>
2025-10-28 17:59:14 +0200

    Minlopro: Project housekeeping activities & CRMA script adjustments

--- scripts/util/crm-analytics/retrieve_metadata_by_wave_app.sh
+++ scripts/util/crm-analytics/retrieve_metadata_by_wave_app.sh
@@ -3,35 +3,34 @@
 # How to use:
 # - bash ./scripts/util/crm-analytics/retrieve_metadata_by_wave_app.sh
 
-# Enable errexit option to exit on command failure
 set -e
 
 # Capture Scratch Org alias;
 read -r -p "🔶 Enter target org alias to pull WAVE metadata from: " TARGET_ORG_ALIAS
 read -r -p "🔶 Enter Wave application API name: " WAVE_APP_NAME
 
-# Override input variables manually for testing purposes!
-# TARGET_ORG_ALIAS="crma-devhub"
-# WAVE_APP_NAME="Minlopro_DX_Analytics"
-
 echo "🔵 Pulling [$WAVE_APP_NAME] app content from [$TARGET_ORG_ALIAS] org..."
 
-# Cleanup working directory
-metadataDir="build/$WAVE_APP_NAME"
-rm -rf "$metadataDir"
-mkdir -p "$metadataDir"
+METADATA_DIR="build/$WAVE_APP_NAME"
 
-# Retrieve metadata by manifest
+cleanup_dir(){
+  rm -rf "$METADATA_DIR"
+  mkdir -p "$METADATA_DIR"
+}
+
+cleanup_dir
+
+# Retrieve all WAVE metadata (by global manifest)
 sf project retrieve start \
     --manifest "./scripts/util/crm-analytics/all-wave-metadata-package.xml" \
     --target-org "$TARGET_ORG_ALIAS" \
-    --output-dir "$metadataDir" \
+    --output-dir "$METADATA_DIR" \
     --ignore-conflicts \
     --wait 10
 
-# Iterate through files in the directory
+# Iterate through metadata files in the directory
 KEYWORD=">$WAVE_APP_NAME</"
-for file in "$metadataDir/wave"/*; do
+for file in "$METADATA_DIR/wave"/*; do
   # Check if the filename contains the keyword
   if grep -q "$KEYWORD" "$file"; then
     echo "Keeping file: $(basename "$file")"
@@ -42,21 +41,18 @@ for file in "$metadataDir/wave"/*; do
 done
 echo "Files have been filtered based on the keyword."
 
-# Compose manifest file with metadata for the WAVE application
-tempManifest="temp/crm-analytics/package.xml"
+# Compose manifest file with metadata for the WAVE application (JS script also depends on "build/wave-app-package.xml" filepath!)
+tempManifest="build/wave-app-package.xml"
 touch "$tempManifest"
-echo "$metadataDir/wave" | node ./scripts/crm-analytics/js/compose_app_manifest_from_metadata.js
+echo "$METADATA_DIR/wave"| node ./scripts/util/crm-analytics/compose_app_manifest_from_metadata.js
 cat "$tempManifest"
 
-# Cleanup working directory (again)
-metadataDir="build/$WAVE_APP_NAME"
-rm -rf "$metadataDir"
-mkdir -p "$metadataDir"
+cleanup_dir
 
 # Initiate retrieve now based on refined manifest file
 sf project retrieve start \
     --manifest "$tempManifest" \
     --target-org "$TARGET_ORG_ALIAS" \
-    --output-dir "$metadataDir" \
+    --output-dir "$METADATA_DIR" \
     --ignore-conflicts \
     --wait 10
