13f05983dc03562f73d8ece3e42c0dd88c3d37d6
Andrii Melnichuk <awesomeandrey@ukr.net>
2025-11-16 17:36:56 +0200

    Minlopro: Embedded 'dorny/test-reporter@v2' to capture & display Jest test results in GHA workflow

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 import { jestConfig } from '@salesforce/sfdx-lwc-jest/config.js';
 
+console.log('Successfully read Jest config file!');
+
 export default {
     ...jestConfig,
     moduleNameMapper: {
         '^lightning/platformShowToastEvent$': '<rootDir>/src/minlopro-core/test/jest-mocks/lightning/platformShowToastEvent'
-    }
+    },
+    reporters: ['default', 'jest-junit']
 };
