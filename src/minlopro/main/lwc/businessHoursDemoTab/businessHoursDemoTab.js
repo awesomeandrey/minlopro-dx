5c9459e6cece682811699f4103164c79bf252f0e
Andrii Melnichuk <awesomeandrey@ukr.net>
2025-11-18 20:18:35 +0200

    Minlopro: Refactored Business Hours demo LWC

--- src/minlopro/main/lwc/businessHoursDemoTab/businessHoursDemoTab.js
+++ src/minlopro/main/lwc/businessHoursDemoTab/businessHoursDemoTab.js
@@ -1,17 +1,36 @@
 import { LightningElement, track, wire } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
-import { isNotEmpty } from 'c/utilities';
+import { cloneObject, isEmpty, parseError } from 'c/utilities';
+import $Toastify from 'c/toastify';
 
 import getDefaultBusinessHoursApex from '@salesforce/apex/BusinessHoursController.getDefaultBusinessHours';
-import calculateMetricsApex from '@salesforce/apex/BusinessHoursController.calculateMetrics';
+import getBusinessHoursApex from '@salesforce/apex/BusinessHoursController.getBusinessHours';
+import calculateMetricsByBusinessHoursApex from '@salesforce/apex/BusinessHoursController.calculateMetricsByBusinessHours';
 
+/**
+ * NOTE: Salesforce does not allow deletion of Business Hours records.
+ * Instead, set Active = FALSE to deactivate them. Keep your org clean by avoiding unnecessary Business Hours.
+ */
 export default class BusinessHoursDemoTab extends LightningElement {
-    @track timezoneKey = 'Europe/Kiev';
+    @track selectedBusinessHoursId = null;
+    @track selectedTimezoneKey = null;
     @track datetime1 = new Date().toISOString();
     @track datetime2 = new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString();
+    @track datetimeMetricsData = [];
 
-    get timezones() {
-        return ['Europe/Kiev', 'America/Los_Angeles', 'America/New_York', 'Europe/London', 'Europe/Paris'].map((key) => ({
+    get selectedBusinessHours() {
+        return this.bhData.find(({ Id }) => Id === this.selectedBusinessHoursId);
+    }
+
+    get businessHours() {
+        return this.bhData.map(({ Id, Name }) => ({
+            label: Name,
+            value: Id
+        }));
+    }
+
+    get businessHoursTimezones() {
+        return this.bhData.map(({ TimeZoneSidKey: key }) => ({
             label: key,
             value: key
         }));
@@ -19,62 +38,147 @@ export default class BusinessHoursDemoTab extends LightningElement {
 
     get stats() {
         return {
-            'Default Business Hours': this.defaultBusinessHoursLabel,
-            'Selected Timezone SID Key': this.timezoneKey,
-            'Date #1 (UTC)': this.datetime1,
-            'Date #2 (UTC)': this.datetime2
+            'Selected Business Hours ID': this.selectedBusinessHoursId,
+            'Selected Business Hours Name': this.selectedBusinessHours?.Name,
+            'Selected Timezone SID Key': this.selectedTimezoneKey,
+            'Date #1 (UTC)': this.datetime1
         };
     }
 
-    get defaultBusinessHoursLabel() {
-        const { data } = this.wiredDefaultBusinessHours;
-        if (isNotEmpty(data)) {
-            return `${data.Name} (${data.TimeZoneSidKey})`;
-        }
-        return null;
-    }
-
     get error() {
-        return this.wiredCalculatedMetrics?.error;
+        return this.wiredDefaultBusinessHours?.error || this.wiredBusinessHours?.error;
     }
 
-    get isDate1BusinessDay() {
-        return this.wiredCalculatedMetrics?.data?.isBusinessDay;
+    get bhColumns() {
+        return [
+            {
+                label: 'Business Hours',
+                fieldName: 'Id',
+                type: 'customLookup',
+                typeAttributes: {
+                    objectApiName: 'BusinessHours'
+                }
+            },
+            {
+                label: 'Time Zone ID',
+                fieldName: 'TimeZoneSidKey',
+                type: 'customBadge'
+            },
+            {
+                label: 'Is Active?',
+                fieldName: 'IsActive',
+                type: 'boolean'
+            },
+            {
+                label: 'Is Default?',
+                fieldName: 'IsDefault',
+                type: 'boolean'
+            }
+        ];
     }
 
-    get date1NextBusinessDay() {
-        return this.wiredCalculatedMetrics?.data?.nextBusinessDay;
+    get bhData() {
+        if (isEmpty(this.wiredBusinessHours.data)) {
+            return [];
+        }
+        return cloneObject(this.wiredBusinessHours.data);
     }
 
-    get date1And2Diff() {
-        const ms = this.wiredCalculatedMetrics?.data?.diff;
-        if (typeof ms !== 'number' || ms < 0) return '0h 0m 0s';
-        const totalSeconds = Math.floor(ms / 1000);
-        const hours = Math.floor(totalSeconds / 3600);
-        const minutes = Math.floor((totalSeconds % 3600) / 60);
-        const seconds = totalSeconds % 60;
-        return `${hours}h ${minutes}m ${seconds}s (${ms})`;
+    get datetimeMetricsColumns() {
+        return [
+            {
+                label: 'Date/Time In UTC Format',
+                fieldName: 'formattedDatetimeUtc',
+                type: 'customCodeSnippet'
+            },
+            {
+                label: 'Target BH',
+                fieldName: 'businessHoursId',
+                type: 'customLookup',
+                typeAttributes: {
+                    objectApiName: 'BusinessHours'
+                }
+            },
+            {
+                label: 'Date/Time In Target BH Format',
+                fieldName: 'formattedDatetimeBh',
+                type: 'customCodeSnippet'
+            },
+            {
+                label: 'Target BH Time Zone ID',
+                fieldName: 'businessHoursTimezone',
+                type: 'customBadge'
+            },
+            {
+                label: 'Is Working Hours?',
+                fieldName: 'isWorkingHours',
+                type: 'boolean'
+            },
+            {
+                label: 'Next Start Date/Time In Target BH Format',
+                fieldName: 'formattedNextStartDatetimeBh',
+                type: 'customCodeSnippet'
+            }
+        ];
     }
 
-    @wire(calculateMetricsApex, { datetime1: '$datetime1', datetime2: '$datetime2' })
-    wiredCalculatedMetrics = {};
+    get disableDatetimeInputs() {
+        return isEmpty(this.selectedBusinessHoursId) || isEmpty(this.selectedTimezoneKey) || isEmpty(this.datetime1);
+    }
 
     @wire(getDefaultBusinessHoursApex)
     wiredDefaultBusinessHours = {};
 
-    handleChangeTimezone(event) {
-        this.timezoneKey = event.detail.value;
+    @wire(getBusinessHoursApex)
+    wiredBusinessHours = {};
+
+    handleChangeBusinessHours(event) {
+        this.selectedBusinessHoursId = event.detail.value;
+        this.selectedTimezoneKey = this.bhData.find(({ Id }) => Id === this.selectedBusinessHoursId)?.TimeZoneSidKey;
     }
 
     handleChangeDate1(event) {
         this.datetime1 = event.detail.value;
     }
 
-    handleChangeDate2(event) {
-        this.datetime2 = event.detail.value;
+    async handleCalculateMetrics(event) {
+        event.preventDefault();
+        this.datetimeMetricsData = [];
+        try {
+            const promises = this.bhData.map(({ Id }) =>
+                calculateMetricsByBusinessHoursApex({
+                    businessHoursId: Id,
+                    datetimeStamp: this.datetime1
+                })
+            );
+            const results = await Promise.all(promises);
+            this.datetimeMetricsData = results.map(
+                ({
+                    businessHours,
+                    datetimeStamp,
+                    formattedDatetimeUtc,
+                    formattedDatetimeBh,
+                    formattedNextStartDatetimeBh,
+                    isWorkingHours
+                }) => ({
+                    businessHoursId: businessHours.Id,
+                    businessHoursTimezone: businessHours.TimeZoneSidKey,
+                    datetimeStamp,
+                    formattedDatetimeUtc,
+                    formattedDatetimeBh,
+                    formattedNextStartDatetimeBh,
+                    isWorkingHours
+                })
+            );
+        } catch (error) {
+            const { message } = parseError(error);
+            $Toastify.error({ message }, this);
+        }
     }
 
     async handleReset() {
-        await refreshApex(this.wiredCalculatedMetrics);
+        await refreshApex(this.wiredDefaultBusinessHours);
+        await refreshApex(this.wiredBusinessHours);
+        this.datetimeMetricsData = [];
     }
 }
