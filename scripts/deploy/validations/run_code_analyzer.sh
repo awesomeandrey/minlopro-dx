79e137557228336cac4461f94b3f8f8914fdeb7c
Andrii Melnichuk <awesomeandrey@ukr.net>
2026-01-26 19:53:29 +0200

    Minlopro: Updated cron expression for 'run-deployment.yml'

--- scripts/deploy/validations/run_code_analyzer.sh
+++ scripts/deploy/validations/run_code_analyzer.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 
-set -e
-
 # How to use:
 # - bash ./scripts/deploy/validations/run_code_analyzer.sh
 # - git diff --name-only develop HEAD | grep -iE "^src" | bash ./scripts/deploy/validations/run_code_analyzer.sh
 
 BUILD_DIR="build"
+FLAGS_DIR="$BUILD_DIR/code-scan-flags-dir"
 SRC_DIR="src"
 
 mkdir -p "$BUILD_DIR"
+rm -rf "$FLAGS_DIR"
+mkdir -p "$FLAGS_DIR"
 
-# Populate default flags
-FLAGS_DIR=$(mktemp -d) && trap 'rm -rf $FLAGS_DIR' EXIT
 touch "$FLAGS_DIR/severity-threshold"; echo "2" > "$FLAGS_DIR/severity-threshold"
+
 touch "$FLAGS_DIR/output-file"; {
   echo "$BUILD_DIR/code-scan-result.csv"
   echo "$BUILD_DIR/code-scan-result.html"
   echo "$BUILD_DIR/code-scan-result.json"
   echo "$BUILD_DIR/code-scan-result.sarif"
 } > "$FLAGS_DIR/output-file"
+
 touch "$FLAGS_DIR/rule-selector"; {
   echo "all:1"
   echo "all:2"
@@ -27,9 +28,8 @@ touch "$FLAGS_DIR/rule-selector"; {
   echo "all:4"
   echo "all:5"
 } > "$FLAGS_DIR/rule-selector"
-touch "$FLAGS_DIR/target"
 
-# Check if stdin has data (-t 0 stands for empty stdin)
+touch "$FLAGS_DIR/target" # Check if stdin has data (-t 0 stands for empty stdin)
 if [[ -t 0 ]]; then
   # Scan whole codebase selecting narrow set of metadata types
   {
