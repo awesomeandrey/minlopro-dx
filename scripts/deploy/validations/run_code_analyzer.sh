4d0d1b9f91999ccb0d36101b33715101b51536cc
Andrii Melnichuk <awesomeandrey@ukr.net>
2026-01-28 18:57:52 +0200

    Minlopro: Refactored 'run_code_analyzer.sh' by switching away from '--flags-dir' approach

--- scripts/deploy/validations/run_code_analyzer.sh
+++ scripts/deploy/validations/run_code_analyzer.sh
@@ -5,48 +5,43 @@
 # - git diff --name-only develop HEAD | grep -iE "^src" | bash ./scripts/deploy/validations/run_code_analyzer.sh
 
 BUILD_DIR="build"
-FLAGS_DIR="$BUILD_DIR/code-scan-flags-dir"
 SRC_DIR="src"
+RESULT_FILENAME="code-scan-result"
 
 mkdir -p "$BUILD_DIR"
-rm -rf "$FLAGS_DIR"
-mkdir -p "$FLAGS_DIR"
 
-touch "$FLAGS_DIR/severity-threshold"; echo "2" > "$FLAGS_DIR/severity-threshold"
+# Build command flags
+CMD_FLAGS=(
+  --severity-threshold 2
+  --output-file "$BUILD_DIR/$RESULT_FILENAME.csv"
+  --output-file "$BUILD_DIR/$RESULT_FILENAME.html"
+  --output-file "$BUILD_DIR/$RESULT_FILENAME.json"
+  --output-file "$BUILD_DIR/$RESULT_FILENAME.sarif"
+  --rule-selector "all:1"
+  --rule-selector "all:2"
+  --rule-selector "all:3"
+  --rule-selector "all:4"
+  --rule-selector "all:5"
+)
 
-touch "$FLAGS_DIR/output-file"; {
-  echo "$BUILD_DIR/code-scan-result.csv"
-  echo "$BUILD_DIR/code-scan-result.html"
-  echo "$BUILD_DIR/code-scan-result.json"
-  echo "$BUILD_DIR/code-scan-result.sarif"
-} > "$FLAGS_DIR/output-file"
-
-touch "$FLAGS_DIR/rule-selector"; {
-  echo "all:1"
-  echo "all:2"
-  echo "all:3"
-  echo "all:4"
-  echo "all:5"
-} > "$FLAGS_DIR/rule-selector"
-
-touch "$FLAGS_DIR/target" # Check if stdin has data (-t 0 stands for empty stdin)
+# Check if stdin has data (-t 0 stands for empty stdin)
 if [[ -t 0 ]]; then
   # Scan whole codebase selecting narrow set of metadata types
-  {
-    echo "$SRC_DIR/**/aura/**"
-    echo "$SRC_DIR/**/classes/**"
-    echo "$SRC_DIR/**/flows/**"
-    echo "$SRC_DIR/**/lwc/**"
-  } > "$FLAGS_DIR/target"
+  CMD_FLAGS+=(
+    --target "$SRC_DIR/**/aura/**"
+    --target "$SRC_DIR/**/classes/**"
+    --target "$SRC_DIR/**/flows/**"
+    --target "$SRC_DIR/**/lwc/**"
+  )
 else
   # Scan passed files only (covering all metadata types within the list)
   while IFS= read -r filepath; do
     if [[ -f "$filepath" ]]; then
-      echo "$filepath" >> "$FLAGS_DIR/target"
+      CMD_FLAGS+=(--target "$filepath")
     else
       echo "Skipping unknown file: $filepath"
     fi
-  done < <(cat) # Take stdin data
+  done
 fi
 
-sf code-analyzer run --flags-dir "$FLAGS_DIR"
+sf code-analyzer run "${CMD_FLAGS[@]}"
