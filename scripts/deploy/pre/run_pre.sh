b5b63521cd3fcb11b47a9504678fd5e5b262ea75
Andrii Melnichuk <awesomeandrey@ukr.net>
2026-09-08 16:13:31 +0300

    Minlopro: Incorporated Einstein Bot into CI pipeline

--- scripts/deploy/pre/run_pre.sh
+++ scripts/deploy/pre/run_pre.sh
@@ -9,7 +9,19 @@ set -e
 read -r -p "🔶 Enter target org alias: " TARGET_ORG_ALIAS
 npx cowsay -W 100 "📗 Running PRE-Deploy Scripts against [$TARGET_ORG_ALIAS] organization..."
 
-# Step 1: Run Custom Shell Scripts;
+# Step 1: Run Custom Apex Scripts
+apexFilesDir="./scripts/deploy/pre/apex"
+if [ -d "$apexFilesDir" ]; then
+    for file in "$apexFilesDir"/*.apex; do
+        if [ -f "$file" ]; then
+            echo "🔵 Running Apex Script [$file] against [$TARGET_ORG_ALIAS] organization..."
+            echo
+            sf apex run --target-org "$TARGET_ORG_ALIAS" --file "$file"
+        fi
+    done
+fi
+
+# Step 2: Run Custom Shell Scripts
 customScriptsDir="./scripts/deploy/pre/custom"
 if [ -d "$customScriptsDir" ]; then
     for file in "$customScriptsDir"/*.sh; do
